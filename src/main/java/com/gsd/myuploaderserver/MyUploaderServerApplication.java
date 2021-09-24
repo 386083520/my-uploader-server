a41218ce9bb56c1d098bb2cdac991cb0509e954a
386083520 <386083520@qq.com>
2021-09-24 22:37:03 +0800

    后端准备

---
+++ src/main/java/com/gsd/myuploaderserver/MyUploaderServerApplication.java
@@ -0,0 +1,13 @@
+package com.gsd.myuploaderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUploaderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUploaderServerApplication.class, args);
+	}
+
+}
